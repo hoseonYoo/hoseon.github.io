b7fe63c1312a5197edb4d7a45bad9f714aedc1a9
hoseonYoo <72132613+hoseonYoo@users.noreply.github.com>
2024-02-14 01:18:50 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//$(function(){
+////        $("body").css({
+////            'background-color':'#000',
+////            'color':'#fff'
+////        });
+////        $("a").css('color','#fff');
+////        $("button").css('color','#fff');
+////        $("header").css('background-color','#000');
+//});
